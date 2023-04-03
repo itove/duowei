59030e1de5b6fa42065e4793134de66ad5b22265
Al Zee <z@alz.ee>
2023-04-03 15:47:51 +0800

    theme duowei

---
+++ web/themes/duowei/js/duowei.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * duowei behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.duowei = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
